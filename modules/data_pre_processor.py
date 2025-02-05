46b702b518a26624ffd5c541f1e3e4312cf1598d
ary@n2001 <76156734+aryan2001atat@users.noreply.github.com>
2025-02-06 01:31:40 +0530

    added text-preprocessor function

---
+++ modules/data_pre_processor.py
@@ -0,0 +1,31 @@
+import nltk
+import string
+import numpy as np
+
+nltk.download('words')
+nltk.download('wordnet')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+nltk.download('maxent_ne_chunker')
+nltk.download('averaged_perceptron_tagger')
+
+from nltk.tag import pos_tag
+from nltk import word_tokenize
+from nltk.chunk import ne_chunk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+def preprocess_text(text):
+    # Tokenize the text
+    tokens = word_tokenize(text.lower())
+
+    # Remove punctuation and stopwords
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word.isalnum() and word not in stop_words]
+
+    # Lemmatize the tokens
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(word) for word in tokens]
+
+    return ' '.join(tokens)
